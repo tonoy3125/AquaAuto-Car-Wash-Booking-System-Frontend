d562ef91cbfb3f9cd701bee13e732ced91ed78b0
tonoy3125 <shaifshajedt@gmail.com>
2024-11-07 23:38:37 +0600

    appointment section design and responsive added

--- src/pages/HomePages/AppointmentBanner/AppointmentBanner.tsx
+++ src/pages/HomePages/AppointmentBanner/AppointmentBanner.tsx
@@ -3,16 +3,16 @@ const AppointmentBanner = () => {
     <div className="w-full pb-56 relative">
       <div className="border relative">
         <img
-          className="w-full h-[40vh] lg:h-[30vh]  object-cover"
+          className="w-full h-[70vh] lg:h-[30vh]  object-cover"
           src="https://i.ibb.co.com/0jzgV10/Rectangle-755-1.png"
           alt=""
         />
         <div className="absolute inset-0 bg-[#0F0452] bg-opacity-40"></div>
         <div className="absolute inset-0 flex flex-col justify-center items-center ">
-          <h3 className="font-poppins font-bold text-4xl text-white text-center mb-1">
+          <h3 className="font-poppins font-bold text-3xl md:text-4xl text-white text-center mb-1">
             Schedule Your appointment today
           </h3>
-          <p className="font-poppins text-base text-white text-center mb-4">
+          <p className="font-poppins semi-sm:text-sm md:text-base text-white text-center mb-4">
             Strateg breed better design, but brand strategies don materialize
           </p>
           <p className="font-poppins text-white text-2xl font-bold text-center">
@@ -22,12 +22,14 @@ const AppointmentBanner = () => {
       </div>
       <div className="absolute right-0 bottom-28">
         <img
+          className="hidden md:block md:w-96 lg:w-full"
           src="https://i.ibb.co.com/fkR54gy/kisspng-2017-ford-mustang-2018-ford-mustang-shelby-mustang-ford-mustang-shelby-gt500-car-5a74aa5a8a8.png"
           alt=""
         />
       </div>
       <div className="absolute left-0 bottom-32">
         <img
+          className="hidden md:block md:w-96 lg:w-full"
           src="https://i.ibb.co.com/MfVL9Pn/kisspng-2017-honda-accord-hybrid-car-honda-s-mx-honda-civi-blue-honda-accord-hybrid-car-5a745a231e81.png"
           alt=""
         />
