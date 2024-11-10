1bb3651b3498b7ccb0e43430c0859c6baa7aafe7
tonoy3125 <shaifshajedt@gmail.com>
2024-11-10 17:40:21 +0600

    Account Banner design added and some chnages commit

---
+++ src/components/AccountBanner/AccountBanner.tsx
@@ -0,0 +1,38 @@
+import { RiArrowDropRightLine } from "react-icons/ri";
+import { NavLink } from "react-router-dom";
+
+const AccountBanner = () => {
+  return (
+    <div className="w-full pb-20 pt-5">
+      <div className="border relative">
+        <img
+          className="w-full h-[70vh] lg:h-[40vh]  object-cover"
+          src="https://i.ibb.co.com/mvgBcC1/About.png"
+          alt=""
+        />
+        <div className="absolute inset-0 bg-black bg-opacity-10"></div>
+        <div className="text-white absolute top-28 md:top-24 lg:top-32  w-full ">
+          <div className="flex flex-col lg:justify-between items-center">
+            <h1 className="text-2xl lg:text-[34px] font-semibold font-poppins mb-5">
+              My Account
+            </h1>
+            <div
+              className="flex items-center text-base gap-1 lg:text-lg font-semibold font-poppins text-[#ffffff]"
+              style={{ lineHeight: "1.6", letterSpacing: "0.025em" }}
+            >
+              <NavLink to="/">
+                <p className=" font-poppins text-base ">Home</p>
+              </NavLink>
+              <span>
+                <RiArrowDropRightLine className="text-3xl font-bold" />
+              </span>
+              <p className="font-poppins text-base ">My Profile</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AccountBanner;
--- src/pages/MyAccount/MyAccount.tsx
+++ src/pages/MyAccount/MyAccount.tsx
@@ -4,6 +4,7 @@ import Navbar from "../Shared/Navbar/Navbar";
 import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 import { logOut, selectCurrentUser } from "@/redux/features/auth/authSlice";
 import { TUserPayload } from "@/types";
+import AccountBanner from "@/components/AccountBanner/AccountBanner";
 
 const MyAccount = () => {
   const navigate = useNavigate();
@@ -19,12 +20,13 @@ const MyAccount = () => {
   return (
     <div>
       <Navbar />
-      <div className="mt-20 mb-20">
+      <AccountBanner />
+      <div className="mt-5 mb-20">
         <h3 className="font-poppins text-lg font-medium text-[#f87f96] text-center mb-4">
           Welcome
         </h3>
         <h1
-          className="text-4xl font-poppins font-medium mb-5 text-center"
+          className="xs:text-2xl sm:text-3xl semi-sm:text-4xl font-poppins font-medium mb-5 text-center"
           style={{ lineHeight: "1", letterSpacing: "0.025em" }}
         >
           {userName}
