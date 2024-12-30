985525effda930fc2b645ba2760753aaf014ae5d
tonoy3125 <shaifshajedt@gmail.com>
2024-12-30 12:50:32 +0600

    team seaction design added commit

--- src/pages/AboutUsPage/AboutUs/AboutUs.tsx
+++ src/pages/AboutUsPage/AboutUs/AboutUs.tsx
@@ -2,6 +2,7 @@ import AboutUsBanner from "@/components/AboutUsBanner/AboutUsBanner";
 import Footer from "../../Shared/Footer/Footer";
 import Navbar from "../../Shared/Navbar/Navbar";
 import Faq from "../Faq/Faq";
+import Team from "../Team/Team";
 
 const AboutUs = () => {
   return (
@@ -9,6 +10,7 @@ const AboutUs = () => {
       <Navbar />
       <AboutUsBanner />
       <Faq />
+      <Team />
       <Footer />
     </div>
   );
