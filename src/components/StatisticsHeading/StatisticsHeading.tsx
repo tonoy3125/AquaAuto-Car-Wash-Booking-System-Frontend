70b6bbd79facded48cd37578e3171f87ba498224
tonoy3125 <shaifshajedt@gmail.com>
2025-03-15 17:28:17 +0600

    statistic heading added

---
+++ src/components/StatisticsHeading/StatisticsHeading.tsx
@@ -0,0 +1,12 @@
+const StatisticsHeading = () => {
+  return (
+    <div className="mb-[20px] font-poppins">
+      <h1 className="text-2xl font-bold">Site Statistics</h1>
+      <p className="text-muted-foreground">
+        overall site statistics-user, payments
+      </p>
+    </div>
+  );
+};
+
+export default StatisticsHeading;
--- src/pages/Dashboard/AdminDashboardPages/AdminDashboard/AdminDashboard.tsx
+++ src/pages/Dashboard/AdminDashboardPages/AdminDashboard/AdminDashboard.tsx
@@ -1,10 +1,11 @@
+import StatisticsHeading from "@/components/StatisticsHeading/StatisticsHeading";
 
 const AdminDashboard = () => {
-    return (
-        <div>
-            <h1>This Is Admin Dashboard</h1>
-        </div>
-    );
+  return (
+    <div className="mt-7 lg:mt-0 md:p-10" style={{ height: "100vh" }}>
+      <StatisticsHeading />
+    </div>
+  );
 };
 
 export default AdminDashboard;
