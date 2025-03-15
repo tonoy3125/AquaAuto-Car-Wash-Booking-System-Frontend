3f5f08751cc4641f214af6873c7d721d4f46dd50
tonoy3125 <shaifshajedt@gmail.com>
2025-03-15 18:29:07 +0600

    daily transaction api add and rechart added

--- src/pages/Dashboard/AdminDashboardPages/AdminDashboard/AdminDashboard.tsx
+++ src/pages/Dashboard/AdminDashboardPages/AdminDashboard/AdminDashboard.tsx
@@ -1,9 +1,11 @@
+import DailyTransactions from "@/components/DailyTransactions/DailyTransactions";
 import StatisticsHeading from "@/components/StatisticsHeading/StatisticsHeading";
 
 const AdminDashboard = () => {
   return (
     <div className="mt-7 lg:mt-0 md:p-10" style={{ height: "100vh" }}>
       <StatisticsHeading />
+      <DailyTransactions />
     </div>
   );
 };
